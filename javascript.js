a50011139328f041c7cb0d5b1c83e5c642f62f25
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-07-25 13:35:06 +0300

    Add 'blank' html, & getComputerChoice func

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// Function return random word from set (rock, papper, scissors)
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    switch (randomNumber) {
+        case 1:
+            console.log("Rock");
+            break;
+        case 2:
+            console.log("Paper");
+            break;
+        case 3:
+            console.log("Scissors");
+            break;
+    }
+}
+
+getComputerChoice();
+
