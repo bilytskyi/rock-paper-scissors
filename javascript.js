772acfb509a361a85180449ea806a76eb07188b2
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-07-25 14:13:48 +0300

    Add playRound func

--- javascript.js
+++ javascript.js
@@ -5,16 +5,51 @@ function getComputerChoice() {
 
     switch (randomNumber) {
         case 1:
-            console.log("Rock");
-            break;
+            return "ROCK";
         case 2:
-            console.log("Paper");
-            break;
+            return "PAPER";
         case 3:
-            console.log("Scissors");
-            break;
+            return "SCISSORS";
     }
 }
 
-getComputerChoice();
+// Function plays 1 round
 
+function playRound(playerSelection, computerSelection) {
+
+    let answer;
+    playerSelection = playerSelection.toUpperCase();
+
+    if (playerSelection == computerSelection) {
+        answer = `You both have ${playerSelection}! This is a DRAW!`;
+        console.log(answer);
+    }
+    else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
+        answer = `You LOSE! ${computerSelection} beats ${playerSelection}`;
+        console.log(answer);
+    }
+    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
+        answer = `You WIN! ${playerSelection} beats ${computerSelection}`;
+        console.log(answer);
+    }
+    else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
+        answer = `You LOSE! ${computerSelection} beats ${playerSelection}`;
+        console.log(answer);
+    }
+    else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+        answer = `You WIN! ${playerSelection} beats ${computerSelection}`;
+        console.log(answer);
+    }
+    else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
+        answer = `You LOSE! ${computerSelection} beats ${playerSelection}`;
+        console.log(answer);
+    }
+    else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+        answer = `You WIN! ${playerSelection} beats ${computerSelection}`;
+        console.log(answer);
+    }
+}
+
+let myChoice = "rOcK";
+
+playRound(myChoice, getComputerChoice());
